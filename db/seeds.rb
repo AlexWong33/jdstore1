98a0f3631f6c8c71ad0c78ca9492c292ad92e2b9
“AlexWong” <and751738167@gmail.com>
2017-05-28 08:45:22 +0800

    only admin can access backend panel

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u = User.new #build a new account
+u.email = "admin@test.com" #email formation
+u.password = "123456"
+u.password_confirmation = "123456" #at least 6 integer
+u.is_admin = true
+u.save
